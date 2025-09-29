9c890a63c287213c427ce093cbe3bd895854e097
msubrandon <167794072+msubrandon@users.noreply.github.com>
2025-09-29 17:18:27 -0500

    RE_Gridded_NADA_Maps.R

--- RE_Gridded_NADA_Maps.R
+++ RE_Gridded_NADA_Maps.R
@@ -1,17 +1,5 @@
-#Maps
+#Map 
 
-
-#This is a map of the entire plot w/contours
-ggplot(nadaxy) +
-  geom_point(aes(lon, lat, color = year), size = 0.1) +
-  geom_contour(aes(lon, lat, z = year)) +
-  theme_minimal() +
-  theme(panel.border = element_rect(color = "black", fill = NA, size = 1))
-
-
-
-### GOOD ONE 
-#Same without Gridpoints
 ggplot(nadaxy) +
   geom_point(aes(lon, lat, color = year), size = .9)+
   theme_minimal() +
